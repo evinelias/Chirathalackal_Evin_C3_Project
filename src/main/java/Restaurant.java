c469b9e70e61e5060d17ebeb52810421a813de1a
evinelias <evinelias@gmail.com>
2023-01-18 00:10:01 +0530

    Part 2 : Solution

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -18,15 +18,21 @@ public class Restaurant {
     }
 
     public boolean isRestaurantOpen() {
-        return true;
-        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+        LocalTime currentTime  =  getCurrentTime();
+        int compareToOpen =  currentTime.compareTo(openingTime);
+        int compareToClose = closingTime.compareTo(currentTime);
+        if((compareToOpen>=0)&&(compareToClose>=0)){
+            return true;
+        }
+        else{
+            return  false;
+        }
     }
 
     public LocalTime getCurrentTime(){ return  LocalTime.now(); }
 
     public List<Item> getMenu() {
-        return null;
-        //DELETE ABOVE RETURN STATEMENT AND WRITE CODE HERE
+        return menu;
     }
 
     private Item findItemByName(String itemName){
